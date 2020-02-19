04abae29ecaf36c10aa88c6e85f13ef002707aed
Paul Gilchrist <paul.gilchrist@outlook.com>
2020-02-19 09:38:12 -0500

    Update Augular to version 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { ConnectivityService } from '../../projects/angular-connectivity/src/public-api';
-import { FilterObjectsPipe, SortObjectsPipe } from '../../projects/angular-pipes-module/src/public-api';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FilterObjectsPipe,
-    SortObjectsPipe
+    AppComponent
   ],
   imports: [
     BrowserModule
