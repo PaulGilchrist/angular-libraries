35c97f0503e180bd6849d1e36ee6d633d3779420
Paul Gilchrist <paul.gilchrist@outlook.com>
2020-11-20 10:30:30 -0500

    Correct linting issues

--- projects/angular-pipes-module/src/lib/sort-objects.pipe.ts
+++ projects/angular-pipes-module/src/lib/sort-objects.pipe.ts
@@ -2,19 +2,20 @@ import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({ name: 'sortObjects' })
 export class SortObjectsPipe implements PipeTransform {
-    // Sort objects either accending or decending by comparing one of the object's properites
-    transform(input: object[], field: string, desc = false): object[] {
-        if (input && field) {
-            return input.sort((a: object, b: object) => {
-                if (a[field] < b[field]) {
-                    return desc ? 1 : -1;
-                }
-                if (b[field] < a[field]) {
-                    return desc ? -1 : 1;
-                }
-                return 0;
-            });
-        }
-        return input;
-    }
+	// Currently can only sort where (typeof input[field] === "string")
+	// Will enhance later to support numbers and dates
+	transform(input, field: string, desc = false) {
+		if (input && field) {
+			return input.sort((a, b) => {
+				if (a[field] < b[field]) {
+					return desc ? 1 : -1;
+				}
+				if (b[field] < a[field]) {
+					return desc ? -1 : 1;
+				}
+				return 0;
+			});
+		}
+		return input;
+	}
 }
